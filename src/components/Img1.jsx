233425652025c5a5b4012befd91a864c210ed53e
valera56 <tenaevv1@gmail.com>
2023-02-07 00:36:50 +0600

    delete_video

--- src/components/Img1.jsx
+++ src/components/Img1.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import video_6 from "../assets/video_6.mp4"
+
 export default function Img1() {
   return (
     <div className="block1">
-        <video className="img1" src={video_6} autoPlay loop muted></video>
-        <video className="img2" src={video_6} autoPlay loop muted></video>
+        
        
-       
+
     </div>
   );
 }
