a0f8e6808dd4c788203ba2ce6d8476d897a129d9
valera56 <tenaevv1@gmail.com>
2023-02-07 00:51:38 +0600

    _img_edit_w

--- src/components/Img1.jsx
+++ src/components/Img1.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Img1() {
   return (
     <div className="block1">
-      <img src="" alt="" />
-      
-      <img src="" alt="" />
+      <img className="img1" src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt="" />
+
+      <img className="img2" src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt="" />
     </div>
   );
 }
