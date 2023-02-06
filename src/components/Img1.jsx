809d7673302c5a2f4a132c4247e02527ff7d6a70
valera56 <tenaevv1@gmail.com>
2023-02-06 10:25:01 +0600

    img-wrap

--- src/components/Img1.jsx
+++ src/components/Img1.jsx
@@ -2,8 +2,8 @@ import React from "react";
 export default function Img1() {
   return (
     <div className="block1">
-      <img src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt=""  className="img1" />
-      <img src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt=""className="img2" />
+      <img src="" alt=""  className="img1" />
+      <img src="" alt=""className="img2" />
     </div>
   );
 }
