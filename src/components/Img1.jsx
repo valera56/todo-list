b262602f6fc8fd0c56f85ec23194bfa84b709bcf
valera56 <tenaevv1@gmail.com>
2023-02-07 00:53:42 +0600

    mi

--- src/components/Img1.jsx
+++ src/components/Img1.jsx
@@ -4,7 +4,6 @@ export default function Img1() {
   return (
     <div className="block1">
       <img className="img1" src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt="" />
-
       <img className="img2" src="https://img1.ak.crunchyroll.com/i/spire2/2d46cd0add8297e8489032ba5f17ceca1669123876_main.jpg" alt="" />
     </div>
   );
